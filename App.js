a0a371220a520ce761f273df47b76f44d0f699ce
akashwebking <139610933+akashwebking@users.noreply.github.com>
2023-09-19 09:06:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Service from './component/Service';
+import Sign from './component/Sign';
+
+function App() {
+  return (
+    <div>
+      <Sign/>
+      <Service/>
+    </div>
+  );
+}
+
+export default App;
